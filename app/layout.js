51180a6c0544056de195344c17f1c2a48959515f
Kushal Das <kushaldas30102002@gmail.com>
2024-01-27 14:10:58 +0530

    changed title and changed readme

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Portfolio Website kushal das",
-  description: "This is my portfolio website kushal das",
+  title: "Kushal Das - Portfolio",
+  description: "This is my portfolio website -  kushal das",
 };
 
 export default function RootLayout({ children }) {
